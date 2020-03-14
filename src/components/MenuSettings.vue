5c9b661f0001e6340b7c87a0376ee711218aa9ea
Bryan Jaramillo <bryan.jaramillo@bitingbit.com>
2020-03-14 20:55:14 +0000

    send folder to vuex from open dialog

---
+++ src/components/MenuSettings.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="button" value="Choose a file" id="select-file" @click="saveNewFolder"/>
+  </div>
+</template>
+
+<script>
+const {dialog} = require("electron").remote;
+import { Folder } from "../Folder/Folder";
+
+export default {
+  name: "MenuSettings",
+  data() {
+    return {
+      a: {}
+    };
+  },
+  methods: {
+    saveNewFolder() {
+      let folder = dialog.showOpenDialogSync({properties: ["openDirectory"]}, (folders) => folders[0]);
+      this.$store.commit("addFolder", new Folder(folder[0]));
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
