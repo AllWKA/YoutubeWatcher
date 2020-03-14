b36dfe3ec7a08604385f30d94a71d79fc789b93f
Bryan Jaramillo <bryan.jaramillo@bitingbit.com>
2020-03-14 02:12:57 +0000

    syncronize the save and electron storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { writeFileSync } from "fs";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
+//TODO: every time we save a vuex, we have to write the json backup
+const electronStoragePath = "./src/store/electronStorage.json";
 export default new Vuex.Store({
   state: {
+    folders: []
   },
   mutations: {
+    addFolder() {
+      this.state.folders.push({for: 5});
+      writeFileSync(electronStoragePath, JSON.stringify(this.state));
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
