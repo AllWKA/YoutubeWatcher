4825fabb3b67b313a0c1625b7209d6f4799156ce
Bryan Jaramillo <bryan.jaramillo@bitingbit.com>
2020-03-14 22:05:14 +0000

    add new components

---
+++ src/components/List.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h3>{{titleList?titleList:"List"}}</h3>
+    <v-treeview
+        v-model="tree"
+        :items="folder"
+        activatable
+        item-key="name"
+        open-on-click
+        expand-icon=""
+        style="text-align: left"
+    >
+      <template v-slot:label="{ item, open }">
+        <div v-if="item.type==='folder'">
+          <v-icon v-if="!item.file">
+            {{ open ? 'keyboard_arrow_down' : 'keyboard_arrow_right' }}
+          </v-icon>
+          <v-icon v-if="!item.file">
+            {{ open ? 'folder_open' : 'folder' }}
+          </v-icon>
+          {{item.name}}
+        </div>
+        <div v-else>
+          <v-icon>
+            music_video
+          </v-icon>
+          {{item.name}}
+        </div>
+
+      </template>
+      <template v-slot:append>
+      </template>
+      <template v-slot:append></template>
+    </v-treeview>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      tree: [],
+      items: []
+    };
+  },
+  props: {
+    folder: Array,
+    titleList: String
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
